78135bbb5586ce2f1142314829f3e7a388854430
Denderson <attackhelicopter688@gmail.com>
2026-06-16 19:46:14 +0200

    Setting up stuff

---
+++ sourceCode/src/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+
+namespace SlugTemplate
+{
+    [BepInPlugin(MOD_ID, "loremiscExpansion", "0.1.0")]
+    [BepInDependency("slime-cubed.slugbase")]
+    class Plugin : BaseUnityPlugin
+    {
+        private const string MOD_ID = "loremiscExpansion";
+
+        // Add hooks
+        public void OnEnable()
+        {
+            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+
+            // Put your custom hooks here!
+
+        }
+        
+        // Load any resources, such as sprites or sounds
+        private void LoadResources(RainWorld rainWorld)
+        {
+        }
+    }
+}
