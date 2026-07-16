cc38bf8b10296664846c85e585827afb9d5e6bcf
Zamtreon <alex.slugcat@gmail.com>
2026-07-16 11:16:13 -0500

    Added base for Collector stats code

---
+++ sourceCode/src/CollectorStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loremiscExpansion
+{
+    public class CollectorStats
+    {
+        EntityID ID;
+        int WanderingScore = 2;
+
+        public CollectorStats(AbstractCreature Host)
+        {
+            ID = Host.ID;
+        }
+    }
+}
